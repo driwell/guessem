77b2bf8bca5cf516d22721b087e4a7fcb768f319
driwell <driwell@tuta.io>
2025-01-06 17:38:44 +0000

    rename x and y variables in to guess and number

--- src/lib.rs
+++ src/lib.rs
@@ -13,8 +13,8 @@ fn generate_number(min: i32, max: i32) -> i32 {
     rand::thread_rng().gen_range(min..=max)
 }
 
-fn get_result(x: &i32, y: &i32) -> String {
-    match x.cmp(y) {
+fn get_result(guess: &i32, number: &i32) -> String {
+    match guess.cmp(number) {
         Ordering::Greater => "Higher".to_string(),
         Ordering::Less => "Lower".to_string(),
         Ordering::Equal => "Correct".to_string(),
