851a4184c65562a85ef93ee20330d8ef72034057
driwell <driwell@tuta.io>
2025-01-06 22:48:23 +0000

    add some color feedback

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,6 @@ use std::cmp::Ordering;
 use bevy::{
     input::keyboard::{Key, KeyboardInput},
     prelude::*,
-    scene::ron::de::Position,
-    sprite::Anchor,
 };
 
 use rand::Rng;
@@ -56,11 +54,13 @@ pub fn keyboard_input_system(
     mut events: EventReader<KeyboardInput>,
     mut text: Query<&mut Text2d, With<Prompt>>,
     mut message: Query<&mut Text2d, (With<Message>, Without<Prompt>)>,
+    mut message_color: Query<&mut TextColor, (With<Message>, Without<Prompt>)>,
     mut guess: Query<&mut Number, With<Player>>,
     mut number: Query<&mut Number, (With<Computer>, Without<Player>)>,
 ) {
     let mut text = text.single_mut();
     let mut message = message.single_mut();
+    let mut message_color = message_color.single_mut();
     let mut guess = guess.single_mut();
     let mut number = number.single_mut();
 
@@ -78,9 +78,16 @@ pub fn keyboard_input_system(
                 guess.0 = text.0.parse::<i32>().unwrap();
 
                 match guess.0.cmp(&number.0) {
-                    Ordering::Less => message.0 = "Higher".to_string(),
-                    Ordering::Greater => message.0 = "Lower".to_string(),
+                    Ordering::Less => {
+                        message_color.0 = Color::srgb_u8(255, 0, 0);
+                        message.0 = "Higher".to_string();
+                    }
+                    Ordering::Greater => {
+                        message_color.0 = Color::srgb_u8(0, 0, 255);
+                        message.0 = "Lower".to_string();
+                    }
                     Ordering::Equal => {
+                        message_color.0 = Color::srgb_u8(0, 255, 0);
                         message.0 = "Correct".to_string();
                         number.0 = rand::thread_rng().gen_range(1..=100);
                     }
