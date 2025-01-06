8c43fe5fefd008b8d06882d9d78dfba3c03bf76d
driwell <driwell@tuta.io>
2025-01-06 17:34:44 +0000

    compare and print results

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,22 @@
+use std::cmp::Ordering;
+
 use rand::Rng;
 
 pub fn play() {
     let number = generate_number(1, 100);
     println!("generated number: {number}");
+    let guess = 42;
+    println!("{}", get_result(&guess, &number));
 }
 
 fn generate_number(min: i32, max: i32) -> i32 {
     rand::thread_rng().gen_range(min..=max)
+}
+
+fn get_result(x: &i32, y: &i32) -> String {
+    match x.cmp(y) {
+        Ordering::Greater => "Higher".to_string(),
+        Ordering::Less => "Lower".to_string(),
+        Ordering::Equal => "Correct".to_string(),
+    }
 }
